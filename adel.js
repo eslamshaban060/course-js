2942cbc716bc11382af3831483330ed77feb6516
eslamshaban060 <eslamshaban060@gmail.com>
2026-03-18 02:48:50 +0200

    iti js lec2

---
+++ adel.js
@@ -0,0 +1,4 @@
+// console.log("from fille tow adel shaban gomah ");
+// function test(a, b) {
+//   return a + b;
+// }
---
+++ eslam.js
@@ -0,0 +1,8 @@
+function test(a, b) {
+  function sub(s, d) {
+    return Math.abs(s, d);
+  }
+  sub();
+  console.log("from fille one eslam shaban gomah ");
+}
+test();
